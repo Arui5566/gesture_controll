c7188482a459fbd74c6c7135861791e566f1a68a
Arui <894683425@qq.com>
2025-11-20 21:01:36 +0800

    feat: add sliding window for stable gesture detection

--- visual_recognize.py
+++ visual_recognize.py
@@ -1,48 +1,40 @@
 import cv2
 from ultralytics import YOLO
-
-import socket
-# 连接 Webots 控制器
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("127.0.0.1", 8000))  # Webots 端监听 8000 端口
+from sliding_window import SlidingWindow
 
 model = YOLO('gestures.pt')
 cap = cv2.VideoCapture(0)
-
-last_cmd = "NONE"
-
-def send_if_changed(cmd):
-    global last_cmd
-    if cmd != last_cmd:
-        client.send(cmd.encode())
-        last_cmd = cmd
+sw = SlidingWindow(window_size=10, stable_threshold=9)
 
 while True:
     ret, frame = cap.read()
     if ret:
         results = model(frame)
-        print(len(results[0].boxes))
-        cv2.imshow('frame', frame)
-        for box in results[0].boxes:
-            cls_id = int(box.cls[0])  # 类别 ID（int）
-            conf = float(box.conf[0])  # 置信度（0~1 float）
-            cls_name = results[0].names[cls_id]  # 类别名称
 
-            if conf >0.9:
-                if cls_name == "like":
-                    send_if_changed("UP")
-                elif cls_name == "dislike":
-                    send_if_changed("DOWN")
-                elif cls_name == "grabbing":
-                    send_if_changed("GRAB")
-                elif cls_name == "palm":
-                    send_if_changed("RELEASE")
-                elif cls_name == "fist":
-                    send_if_changed("STOP")
+        boxes = results[0].boxes
 
-            print(f"类别: {cls_name}, 置信度: {conf:.2f}")
-        annotated_frame = results[0].plot()  # BGR numpy array
+        if len(boxes) > 0:
+            best_box = max(boxes, key=lambda b: float(b.conf[0]))
 
+            cls_id = int(best_box.cls[0])
+            conf = float(best_box.conf[0])
+            cls_name = results[0].names[cls_id]
+
+            if conf > 0.9:
+                sw.add(cls_name)
+
+            annotated_frame = results[0].plot(
+                boxes=[best_box]
+            )
+        else:
+            annotated_frame = frame
+
+        stable_gesture = sw.get_stable()
+
+
+        cv2.putText(annotated_frame, f'Stable: {stable_gesture}',
+                    (20, 40), cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (0, 255, 0), 2)
         cv2.imshow('frame', annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
